f67e6da4e112060314348412ef7d10069f652f60
delete44 <delete44+tech@protonmail.com>
2022-12-06 19:25:29 +0000

    Formatted files

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
+const CHARACTER_LIMIT = 500;
+
 const convertClicked = () => {
-  const textInput = document.getElementById("text-input")
+  const textInput = document.getElementById("text-input");
   const text = textInput.value;
 
-  textInput.value = "SingsNote " + text
-                      .replace(/\\*\n+/g, "\\\n")
-                      .replace(/SingsNote\s*/g, "")
-                      .replace(/\s{2,}/g, " ")
+  textInput.value =
+    "SingsNote " +
+    text
+      .replace(/\\*\n+/g, "\\\n")
+      .replace(/SingsNote\s*/g, "")
+      .replace(/\s{2,}/g, " ");
 
-}
+  textChanged();
+};
 
 const copyClicked = () => {
   var copyText = document.getElementById("text-input");
@@ -16,4 +21,23 @@ const copyClicked = () => {
   copyText.setSelectionRange(0, 99999);
 
   navigator.clipboard.writeText(copyText.value);
-}
+};
+
+const textChanged = () => {
+  const textInput = document.getElementById("text-input");
+  const counterElement = document.getElementById("counter");
+
+  const characterCount = textInput.value.length;
+
+  counterElement.innerHTML = characterCount;
+  if (characterCount > CHARACTER_LIMIT) {
+    counterElement.classList.add("danger");
+  } else {
+    counterElement.classList.remove("danger");
+  }
+};
+
+textChanged();
+
+const maxElement = document.getElementById("max");
+maxElement.innerHTML = CHARACTER_LIMIT;
