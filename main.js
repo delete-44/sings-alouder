3e03b0e7ccc92cc63d8ae37ddc3a563f1561b41c
delete44 <delete44+tech@protonmail.com>
2022-12-06 18:38:27 +0000

    Added "copy to clipboard" button; minor UI updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const convertClicked = () => {
-  const textInput = document.getElementById('text-input')
+  const textInput = document.getElementById("text-input")
   const text = textInput.value;
 
   textInput.value = "SingsNote " + text
@@ -7,4 +7,13 @@ const convertClicked = () => {
                       .replace(/SingsNote/g, "")
                       .replace(/\s{2,}/g, " ")
 
+}
+
+const copyClicked = () => {
+  var copyText = document.getElementById("text-input");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
 }
