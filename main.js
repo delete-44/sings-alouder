e89fea1a880f39e03ff96f1e541bed0784b8b94d
delete44 <delete44+tech@protonmail.com>
2022-12-06 17:39:18 +0000

    Removing duplicate newlines

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const convertClicked = () => {
   const text = textInput.value;
 
   textInput.value = "SingsNote " + text
-                      .replace(/\\*\n/g, "\\\n")
+                      .replace(/\\*\n{2,}/g, "\\\n")
                       .replace(/SingsNote/g, "")
                       .replace(/\s{2,}/g, " ")
 
